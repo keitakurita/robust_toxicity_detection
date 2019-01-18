f65b72f3b41c77d540bbec31d428f7d908a4f111
keitakurita <keita.kurita@gmail.com>
2019-01-18 14:10:43 -0500

    NEW: Added experiment recording script

---
+++ scripts/record_experiments.py
@@ -0,0 +1,42 @@
+from typing import *
+import pymongo
+from bson.objectid import ObjectId
+import os
+import logging
+
+# Logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s - %(name)s %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+PASSWORD = "mongo11747" #  not good practice, but this is a private repo so...
+
+conn_str = f"mongodb+srv://root:{PASSWORD}@cluster0-ptgoc.mongodb.net/test?retryWrites=true"
+
+client = pymongo.MongoClient(conn_str)
+db = client.experiments
+collection = db.logs
+
+def record(log: dict):
+    res = collection.insert_one(log)
+    logger.info(f"Inserted results at id {res.inserted_id}")
+    return res
+
+def find(id_: Optional[str]=None, query: Optional[dict]=None):
+    if query is None: query = {"_id": ObjectId(id_)}
+    res = collection.find_one(query)
+    return res
+
+def delete(id_: Optional[str]=None, query: Optional[dict]=None):
+    if query is None: query = {"_id": ObjectId(id_)}
+    res = collection.delete_many(query)
+    logger.info(f"Deleted {res.deleted_count} entries")
+    return res
+
+if __name__ == "__main__":
+    res = record({"hoge": "foo"})
+    delete(id_=res.inserted_id)
