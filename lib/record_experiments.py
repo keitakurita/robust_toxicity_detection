77719d99a33515b99b91cc21b2fe812ce8cbb301
keitakurita <keita.kurita@gmail.com>
2019-01-30 21:21:07 -0500

    BUG: Fixed mongodb serialization bug by converting all numpy int/floats to python int/floats

--- lib/record_experiments.py
+++ lib/record_experiments.py
@@ -21,8 +21,19 @@ client = pymongo.MongoClient(conn_str)
 db = client.experiments
 collection = db.logs
 
+def _cln(v: Any) -> Any:
+    """Ensure variables are serializable"""
+    if isinstance(v, (np.float, np.float16, np.float32, np.float64, np.float128)):
+        return float(v)
+    elif isinstance(v, (np.int, np.int0, np.int8, np.int16, np.int32, np.int64)):
+        return int(v)
+    elif isinstance(v, dict):
+        return {k: _cln(v_) for k, v_ in v.items()}
+    else:
+        return v
+
 def record(log: dict):
-    res = collection.insert_one({k: str(v) for k, v in log.items()})
+    res = collection.insert_one({str(k): _cln(v) for k, v in log.items()})
     logger.info(f"Inserted results at id {res.inserted_id}")
     return res
 
